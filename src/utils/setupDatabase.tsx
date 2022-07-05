0e922e47f3dfdaf83672d8fd5508ba9faea96685
Vikms95 <vms1995@hotmail.com>
2022-07-05 17:25:15 +0200

    Set up Firebase

---
+++ src/utils/setupDatabase.tsx
@@ -0,0 +1,15 @@
+// Import the functions we need from the SDKs
+import { initializeApp } from 'firebase/app';
+
+// Web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: 'AIzaSyARIOjJhFouHKja-Aj7SkSphdZUlOVI2XQ',
+	authDomain: 'react-twitter-clone-b954b.firebaseapp.com',
+	projectId: 'react-twitter-clone-b954b',
+	storageBucket: 'react-twitter-clone-b954b.appspot.com',
+	messagingSenderId: '320229846649',
+	appId: '1:320229846649:web:c53d536b7886bdebf7aea8',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
